749fe9961b91e6d9d040f73660d5d36ced85b449
CrastaRyan <ryan.crasta@objectedge.com>
2024-03-30 12:51:20 +0530

    netlify build

--- src/app/api/trpc/[trpc]/route.js
+++ src/app/api/trpc/[trpc]/route.js
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { NextRequest } from "next/server";
-import { env } from "~/env";
+import { env } from "../../../../env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
